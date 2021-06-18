869d5553bfa48116b1ad605d8dc5b79805413d98
sholost <stoedirman@gmail.com>
2021-06-18 16:08:30 +0700

    start bgt

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
+import Navbar from './components/Navbar'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to kohisop</h1>
+      <label htmlFor="serach"> Search: </label>
+      <input id="search" type="text"></input>
+      <div>Your Location : <b>SLEMAN</b></div>
+      <label>Set Filters</label>
+      <button> SET </button>
+      <div className="container">
+      </div>
     </div>
   );
 }
