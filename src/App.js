71179882a5ad1084bef68c201c8de137c706d395
sholost <stoedirman@gmail.com>
2021-06-18 20:18:10 +0700

    cek bootstrap

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
-import logo from './logo.svg';
-import Navbar from './components/Navbar'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './App.css';
+import React from "react";
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to kohisop</h1>
-      <label htmlFor="serach"> Search: </label>
-      <input id="search" type="text"></input>
-      <div>Your Location : <b>SLEMAN</b></div>
-      <label>Set Filters</label>
-      <button> SET </button>
-      <div className="container">
+      <div class="card-body">
+        <label htmlFor="serach"> Search: </label>
+        <input id="search" type="text"></input>
+        <div>Your Location : <b>SLEMAN</b></div>
+        <label>Set Filters</label>
+        <button class="btn btn-primary">SET</button>
       </div>
     </div>
   );
